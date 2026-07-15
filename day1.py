596eb8556a5c3383c1d6804a16e4dd23dffc2178
Karthika RP <karthukarthik242@gmail.com>
2026-07-16 04:18:30 +1030

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,25 @@
+#creating a list 
+
+number = [10,20,30,40]
+print(number)
+
+# creating the empty list
+
+marks = []
+print(marks)
+
+#creating the value with the range
+
+numbers = list(range(1,6))
+
+print(numbers)
+
+# Accessing the elements throng the index
+
+values = list(range(1,5))
+
+print(values)
+
+print(values[0])
+print(values[3])
+print(values[3])
