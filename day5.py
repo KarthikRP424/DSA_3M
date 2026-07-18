1b0a3e5a468207bdd2eea4d11a0f7d675e05afbe
Karthika RP <karthukarthik242@gmail.com>
2026-07-19 04:05:01 +1030

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,13 @@
+name = "karthik"
+
+target = "t"
+
+for index, character in enumerate(name):
+    
+    if target == character:
+        
+        print("the target is found")
+        break
+        
+    else:
+        print("the target is not found ")
