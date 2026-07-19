eadcedaf654b1ae0a5e4c952bf00dbdf11eb1017
Karthika RP <karthukarthik242@gmail.com>
2026-07-20 04:12:41 +1030

    Add files via upload

---
+++ day6,1.py
@@ -0,0 +1,8 @@
+#Method2 of Array reverse 
+
+
+marks = [1,2,3,4,5]
+
+marks.reverse()
+
+print(marks)
---
+++ day6.py
@@ -0,0 +1,5 @@
+array = [10,20,30,40,50]
+
+reverse_array = array[::-1]
+
+print(reverse_array)
