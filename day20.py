a6227e76164c5ad57544e1976ba409e3916088f8
Karthika RP <karthukarthik242@gmail.com>
2026-08-21 03:08:50 +1030

    Add files via upload

---
+++ day20.py
@@ -0,0 +1,35 @@
+# to creating an array
+a = []
+# a.append(10)
+# a.append(20)
+# a.append(19.0)
+# a.append('karthik')
+# print(a)
+# for fetching the element 
+a.append(10)
+a.append(22)
+a.append(450)
+a.append(78)
+# print(a[1])
+# # for updating the value
+# a[2] = 'ps'
+# a[1] = 'rgdb'
+# print(a)
+# # for deleting the element use pop method
+# a.pop(3)
+# print(a)
+# 1 find the largest element using max
+x = max(a)
+print(x)
+# 2 find the min value
+y = min(a)
+print(y)
+# searching the element is there are not 
+z = 22 in a
+print(z)
+# for sorting 
+a.sort()
+print(a)
+# for decreasing order
+a = a[::-1]
+print(a)
