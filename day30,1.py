7cedc85d0b6b21df25c616a4fa81cd10777d086c
Karthika RP <karthukarthik242@gmail.com>
2026-08-31 03:52:45 +1030

    Add files via upload

---
+++ day30,1.py
@@ -0,0 +1,16 @@
+h = {}
+
+h[1] = "one"
+h[2] = "two"
+h[3] = "three"
+h[4] = "four"
+h[5] = "five"
+
+print(h.keys())
+
+print(h.values())
+print(h)
+# print(h.items())
+
+print(1 in h.keys())
+print(6 in h.keys())
